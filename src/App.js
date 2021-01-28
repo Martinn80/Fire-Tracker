944223a52c5bcadffffc1cb54b486bba1b715133
Martin Nguyen <ngumartin@gmail.com>
2021-01-28 15:28:37 -0800

    styled the locationinfobox and added and styled header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import Map from './components/Map'
 import { useState, useEffect } from 'react'
 import Loader from './components/Loader'
+import Header from './components/Header'
 
 function App() {
-    const [eventData, setEventData] = useState([])
-    const [loading, setLoading] = useState(false)
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-      const fetchEvents = async () => {
-        setLoading(true)
-        const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
-        const { events } = await res.json()
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
 
-        setEventData(events)
-        setLoading(false)
+      setEventData(events)
+      setLoading(false)
 
-      }
+    }
 
-      fetchEvents()
-      // console.log(eventData)
-    }, [])
+    fetchEvents()
+    // console.log(eventData)
+  }, [])
 
   return (
     <div>
-      { !loading ? <Map eventData={eventData} /> : <Loader /> }
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
