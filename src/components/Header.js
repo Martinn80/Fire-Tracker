5f14e60c5f8f1c33aa28d92dc2769b947f5f481c
Martin Nguyen <ngumartin@gmail.com>
2021-01-29 12:29:49 -0800

    added style and color to theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react'
 const Header = () => {
     return (
         <header className='header'>
-            <h1><Icon icon={locationIcon} /> Wildire Tracker (Powered By: NASA)</h1>
+            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered By: NASA)</h1>
 
         </header>
     )
